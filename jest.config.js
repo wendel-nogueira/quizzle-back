742c5c99faf6455f5c830aea2230ba5f20f62099
Wendel Nogueira <43501645+wendel-nogueira@users.noreply.github.com>
2023-07-12 20:07:19 -0300

    FIX: fix deploy

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.(ts|js)'],
+    // testMatch: ['**/tests/**/*.test.(ts|js)'],
+    testMatch: ['**/tests/question/deleteQuestion.test.(ts|js)'],
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
     },
